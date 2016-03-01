0b32fceef45d853c384afed5dd86108ee25fb346
Maksim <maxkvt@gmail.com>
2016-03-01 22:59:04 +0500

    PizzaSolution

---
+++ PizzaSolution/engine.js
@@ -0,0 +1,40 @@
+var pizzas;
+function handleFileSelect(evt) {
+    var files = evt.target.files; // FileList object
+
+    // Loop through the FileList and render image files as thumbnails.
+    for (var i = 0, f; f = files[i]; i++) {
+
+      // Only process image files.
+      if (!f.type.match('image.*')) {
+        continue;
+      }
+
+      var reader = new FileReader();
+
+      // Closure to capture the file information.
+      reader.onload = (function(theFile) {
+        return function(e) {
+          // Render thumbnail.
+          var span = document.createElement('span');
+          span.innerHTML = ['<img class="thumb" src="', e.target.result,
+                            '" title="', escape(theFile.name), '"/>'].join('');
+          document.getElementById('list').insertBefore(span, null);
+        };
+      })(f);
+
+      // Read in the image file as a data URL.
+      reader.readAsDataURL(f);
+    }
+  }
+
+function init() {
+        if (window.File && window.FileReader && window.FileList && window.Blob) {
+  	// Great success! All the File APIs are supported.
+	} else {
+  		alert('The File APIs are not fully supported in this browser.');
+		}
+
+	document.getElementById('files')
+		.addEventListener('change', handleFileSelect, false);        
+}
